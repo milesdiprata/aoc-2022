5951d73b5f7cc0015f6446829690b595b9f45563
Miles di Prata <milesdiprata3@gmail.com>
2026-02-14 13:38:32 -0800

    Day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,4 @@
+use std::collections::BinaryHeap;
 use std::fs;
 use std::str::FromStr;
 use std::time::Instant;
@@ -33,6 +34,15 @@ fn part1(elves: &[Elf]) -> u64 {
     elves.iter().map(Elf::total).max().unwrap_or_default()
 }
 
+fn part2(elves: &[Elf]) -> u64 {
+    let mut calories = elves.iter().map(Elf::total).collect::<BinaryHeap<_>>();
+
+    (0..3)
+        .map(|_| calories.pop())
+        .map(Option::unwrap_or_default)
+        .sum()
+}
+
 fn main() -> Result<()> {
     let elves = fs::read_to_string("in/day1.txt")?
         .split("\n\n")
@@ -46,6 +56,15 @@ fn main() -> Result<()> {
 
         println!("Part 1: {part1} ({elapsed:?})");
         assert_eq!(part1, 72_070);
+    };
+
+    {
+        let start = Instant::now();
+        let part2 = self::part2(&elves);
+        let elapsed = Instant::now().duration_since(start);
+
+        println!("Part 2: {part2} ({elapsed:?})");
+        assert_eq!(part2, 211_805);
     };
 
     Ok(())
