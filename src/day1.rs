b3c18e1bc705cf264cf82821eb2f619728d11326
Miles di Prata <milesdiprata3@gmail.com>
2026-02-14 13:36:36 -0800

    Day 1 part 1

---
+++ src/day1.rs
@@ -0,0 +1,52 @@
+use std::fs;
+use std::str::FromStr;
+use std::time::Instant;
+
+use anyhow::Error;
+use anyhow::Result;
+
+#[derive(Debug)]
+struct Elf {
+    calories: Vec<u32>,
+}
+
+impl FromStr for Elf {
+    type Err = Error;
+
+    fn from_str(calories: &str) -> Result<Self> {
+        Ok(Self {
+            calories: calories
+                .lines()
+                .map(str::parse)
+                .collect::<Result<Vec<_>, _>>()?,
+        })
+    }
+}
+
+impl Elf {
+    fn total(&self) -> u64 {
+        self.calories.iter().copied().map(u64::from).sum()
+    }
+}
+
+fn part1(elves: &[Elf]) -> u64 {
+    elves.iter().map(Elf::total).max().unwrap_or_default()
+}
+
+fn main() -> Result<()> {
+    let elves = fs::read_to_string("in/day1.txt")?
+        .split("\n\n")
+        .map(Elf::from_str)
+        .collect::<Result<Vec<_>>>()?;
+
+    {
+        let start = Instant::now();
+        let part1 = self::part1(&elves);
+        let elapsed = Instant::now().duration_since(start);
+
+        println!("Part 1: {part1} ({elapsed:?})");
+        assert_eq!(part1, 72_070);
+    };
+
+    Ok(())
+}
